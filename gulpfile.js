27fbdac52829c9dbd74f358eac76465f458454ff
UnaHermosa <veintitress@yandex.ru>
2024-12-01 19:15:51 +0300

    Закончена разметка главной страницы

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,41 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat-css');
-const del = require('del');
 const plumber = require('gulp-plumber');
+const del = require('del');
 const browserSync = require('browser-sync').create();
-const build = gulp.series(clean, gulp.parallel(html, css, images));
-const watchapp = gulp.parallel(build, watchFiles, serve);
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+}
 
 function html() {
   return gulp.src('src/**/*.html')
-    .pipe(plumber())
-    .pipe(gulp.dest('dist/'))
-    .pipe(browserSync.reload({stream: true}));
+        .pipe(plumber())
+				.pipe(gulp.dest('dist/'))
+        .pipe(browserSync.reload({stream: true}));
 }
 
 function css() {
   return gulp.src('src/blocks/**/*.css')
-    .pipe(plumber())
-    .pipe(concat('bundle.css'))
-    .pipe(gulp.dest('dist/'))
-    .pipe(browserSync.reload({stream: true}));
+        .pipe(plumber())
+        .pipe(concat('bundle.css'))
+				.pipe(gulp.dest('dist/'))
+        .pipe(browserSync.reload({stream: true}));
 }
 
 function images() {
-  return gulp.src('src/images/**/*.{jpg, png, jpeg, svg, avif, webp,gif, ico}')
+  return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}', {encoging: false})
     .pipe(gulp.dest('dist/images'))
     .pipe(browserSync.reload({stream: true}));
 }
 
 function fonts() {
-  return gulp.src('src/fonts/**/*.{woff, woff2, ttf, woff2-variations}')
+  return gulp.src('src/fonts/**/*.{ttf, woff, woff2}')
+    .pipe(plumber())
     .pipe(gulp.dest('dist/fonts'))
     .pipe(browserSync.reload({stream: true}));
 }
@@ -40,23 +47,19 @@ function clean() {
 function watchFiles() {
   gulp.watch(['src/**/*.html'], html);
   gulp.watch(['src/blocks/**/*.css'], css);
-  gulp.watch(['src/images/**/*.{jpg, png, jpeg, svg, avif, webp,gif, ico}'], images);
-  gulp.watch(['src/fonts/**/*.{woff, woff2, ttf, woff2-variations}'], fonts)
+  gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(['src/fonts/**/*.{ttf, woff, woff2'], fonts);
 }
 
-function serve() {
-  browserSync.init({
-    server: {
-      baseDir: './dist'
-    }
-  });
-}
+const build = gulp.series(clean, gulp.parallel(html, css, images, fonts));
+const watchapp = gulp.parallel(build, watchFiles, serve);
 
 exports.html = html;
 exports.css = css;
 exports.images = images;
-exports.fonts = fonts;
 exports.clean = clean;
+exports.fonts = fonts;
+
 exports.build = build;
 exports.watchapp = watchapp;
 exports.default = watchapp;
